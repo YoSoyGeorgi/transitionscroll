cbbe164df826e27005dce193dd6bba7ed8cca8a3
Jorge Luis Olvera <holayosoygeorgi@outlook.com>
2023-11-27 11:13:47 -0600

    cards en nosotros

--- nosotros.php
+++ nosotros.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nosotros</title>
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="styles/stylenos.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
@@ -67,76 +66,54 @@
 
 
     <div class="container">
+        <div class="card">
+            <h2>Tarjeta 1</h2>
+            <p>Este es el contenido de la tarjeta 1.</p>
+        </div>
 
-        <section class="myv">
-            <div data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine" class="mision">
-                <h1>Misión</h1>
-                <p>Brindar servicios de capacitación,
-                    asesoría y consultoría educativa de
-                    manera comprometida y práctica,
-                    acompañando a las instituciones a
-                    alcanzar sus objetivos, buscando la
-                    innovación.</p>
-            </div>
+        <div class="card">
+            <h2>Tarjeta 2</h2>
+            <p>Este es el contenido de la tarjeta 2.</p>
+        </div>
 
-            <div data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine" class="vision">
-                <h1>Visión</h1>
-                <p>Posicionarnos entre las empresas de
-                    formación con mejores referencias a
-                    nivel nacional, profesionalizando a todos
-                    aquellos vinculados al área de
-                    enseñanza y de capacitación</p>
-            </div>
-
-        </section>
-
-    
-
-        <section class="historia">
-            <div class="hist">
-                <h2>historia</h2>
-            </div>
-        </section>
-
-
-
-        <footer class="pie-pag">
-
-            <div class="grupo-1">
-                <div class="box">
-                    <figure>
-                        <a href="#">
-                            <img src="img/ense.png" alt="Logo">
-                        </a>
-                    </figure>
-                </div>
-                <div class="box">
-                    <h2>Contactanos</h2>
-                    <p>informes@ensenandoaensenar.com</p>
-                    <p>442 894 0842</p>
-                </div>
-                <div class="box">
-                    <h2>Siguenos</h2>
-                    <div class="red-social">
-                        <a href="#" class='bx bxl-facebook-circle bx-tada'></a>
-                        <a href="#" class='bx bxl-instagram bx-tada'></a>
-                        <a href="#" class='bx bxl-youtube bx-tada'></a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="grupo-2">
-                <small>&copy; 2023 <b>Enseñando a Enseñar</b> - Derechos Reservados</small>
-            </div>
-
-        </footer>
+        <div class="card">
+            <h2>Tarjeta 3</h2>
+            <p>Este es el contenido de la tarjeta 3.</p>
+        </div>
     </div>
 
-</body>
 
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<script>
-    AOS.init();
-</script>
+    <footer class="pie-pag">
+
+        <div class="grupo-1">
+            <div class="box">
+                <figure>
+                    <a href="#">
+                        <img src="img/ense.png" alt="Logo">
+                    </a>
+                </figure>
+            </div>
+            <div class="box">
+                <h2>Contactanos</h2>
+                <p>informes@ensenandoaensenar.com</p>
+                <p>442 894 0842</p>
+            </div>
+            <div class="box">
+                <h2>Siguenos</h2>
+                <div class="red-social">
+                    <a href="#" class='bx bxl-facebook-circle bx-tada'></a>
+                    <a href="#" class='bx bxl-instagram bx-tada'></a>
+                    <a href="#" class='bx bxl-youtube bx-tada'></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="grupo-2">
+            <small>&copy; 2023 <b>Enseñando a Enseñar</b> - Derechos Reservados</small>
+        </div>
+
+    </footer>
+
+</body>
 
 </html>
